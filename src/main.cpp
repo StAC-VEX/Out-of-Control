ab5846e9c05d13b346c8efac00b5591584093b57
Charps-Studios <>
2021-03-24 17:52:31 +1300

    Added documentation, changed fuction arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * The starting off point of the program.
+ * 
+ * @author Bailey Moir
+ */
+
 #include "vex.h"
 using namespace vex;
 
@@ -13,18 +19,46 @@ void vexcodeInit() {
   sensor.calibrate();  
 }
 
+/**
+ * The class that contains all of the autonomous functions.
+ */
 class Auton { 
 public:
+  /**
+   * Turns the robot a specified amount.
+   * 
+   * @param degrees The amount of degress the robot will turn.
+   */
   void turn(int degrees) {
 
   }
+
+  /**
+   * Moves the robot for a specificed distance.
+   * 
+   * @param move How far you want the robot to move and what direction (positive is forward, negative is backwards).
+   */
   void move(int distance) {
 
+  /**
+   * Moves the robot for a specified amount of time at a specified speed.
+   * 
+   * @param time The miliseconds (amount of time) that you want the robot to move forward.  
+   * @param dps The amount of degrees per second you want the robot to move (positive is forward, negative is backwards).
+   */
   }
-  void move(int time) {
+  void move(int time, int dps) {
 
   }
-  void intake(motor* motors, int time) {
+
+  /**
+   * Intakes the specified motors for a specified amount of time at a specified speed.
+   * 
+   * @param time The miliseconds (amount of time) to spin the motors for.
+   * @param dps The degrees per second to spin the motors at.
+   * @param motors The list of motors to spin.
+   */
+  void intake(int time, int dps, motor* motors) {
 
   }
 };
