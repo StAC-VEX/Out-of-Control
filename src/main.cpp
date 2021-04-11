68bba82831513b72882ce9c4e7158bc93c903d8a
Charps-Studios <>
2021-04-11 14:58:34 +1200

    Added the auton instructions, added png version.

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,71 @@ void auton() {
   * Pick up oranges
   * Put purple on top
   * Dump orange ones, then go to opposite and do the same thing.
-  */
+  */ 
+  startIntake(720);
+  moveTime(1500, 720);
+  stopIntake();
+  //Collected first purple cube,
+  move(-(60.69 - distanceToFront));
+  turn(-90);
+  moveTimeDistance(60.69 - distanceToFront, 720);
+  intake(500, 720);
+  move(-cubeWidth);
+  moveSideWays(-31.845);
+  moveTimeDistance(cubeWidth, 720);
+  intake(500, 720);
+  move(-cubeWidth);
+  moveSideWays(-31.845);
+  moveTimeDistance(cubeWidth, 720);
+  intake(500, 720);
+  // Collected all orange cubes.
+  moveTime(100, 720);
+  move(-(121.38 - distanceToFront));
+  turn(45);
+  move(-64.37);
+  intake(1500, 720);
+  // Purple cube deposited.
+  move(64.37);
+  turn(45);
+  move(262.76);
+  turn(90);
+  startIntake(-720);
+  move(-80.69);
+  stopIntake();
+  // Orange cubes deposited.
+  turn(90);
+  startIntake(720);
+  moveTimeDistance(161.725 - cubeWidth, 720);
+  stopIntake();
+  // Next purple cube collected.
+  move(-60.69);
+  turn(90);
+  moveTimeDistance(60.69 - distanceToFront, 720);
+  intake(500, 720);
+  move(-cubeWidth);
+  moveSideWays(31.845);
+  moveTimeDistance(cubeWidth, 720);
+  intake(500, 720);
+  move(-cubeWidth);
+  moveSideWays(31.845);
+  moveTimeDistance(cubeWidth, 720);
+  intake(500, 720);
+  // Collected all green cubes.
+  moveTime(100, 720);
+  move(-(121.38 - distanceToFront));
+  turn(-45);
+  move(-64.37);
+  intake(1500, 720);
+  // Last purple cube deposited.
+  move(64.37);
+  turn(-45);
+  move(262.76);
+  turn(-90);
+  startIntake(-720);
+  moveTime(5000, -720);
+  stopIntake();
+  //Everything deposited.
+
 }
 
 /**
